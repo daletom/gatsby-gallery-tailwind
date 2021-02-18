789025a87fb876e9ed56041f414582d77867a3c8
daletom <tom@imgix.com>
2021-02-18 07:40:57 -0800

    change host to local

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://localhost:8888/xigmi/graphql`,
+          `http://gatsbygallerytailwind.local/graphql`,
       },
     },
     `gatsby-plugin-postcss`,
